543b66fc882da04db71e91453765750132a5c046
Sheldon Lobo <sheldon.lobo@oracle.com>
2020-09-28 23:33:47 -0400

    * Query to find the title for the input series ID. * Add more examples to the README

--- README.md
+++ README.md
@@ -10,6 +10,9 @@ Java query FRED data, parse with Jackson, draw a graph with JFreeChart.
 # Examples
 - `java -jar target/fred-graph-*-jar-with-dependencies.jar POP`
 - `java -jar target/fred-graph-*-jar-with-dependencies.jar GNPCA POP`
+- `java -jar target/fred-graph-*-jar-with-dependencies.jar NYLF CALF TXLF`
+- `java -jar target/fred-graph-*-jar-with-dependencies.jar NYNAN CANAN TXNAN`
+- `java -jar target/fred-graph-*-jar-with-dependencies.jar NYUR NJUR NCUR CAUR TXUR`
 
 # References
 - [FRED API](https://fred.stlouisfed.org/docs/api/fred/)
--- src/main/java/org/lobo/fred/graph/QueryFred.java
+++ src/main/java/org/lobo/fred/graph/QueryFred.java
@@ -13,26 +13,64 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class QueryFred {
+    private static final String FRED_API = "https://api.stlouisfed.org/fred/";
+    private static final String API_KEY = "e12c0787f51ff6db24ac8029710fa175";
+
     protected  static Map<String, Map<String, Double>> getAllData(String[] seriesArray) {
         HashMap<String, Map<String, Double>> dataMaps = new HashMap<>();
 
         // Iterate over all input args, returning FRED queried data for each
         for (String series : seriesArray) {
             System.out.println("Getting data for: " + series);
-            dataMaps.put(series, getData(series));
+            dataMaps.put(getTitle(series) + " (" + series + ")", getData(series));
         }
 
         return dataMaps;
+    }
+
+    private static String getTitle(String seriesId) {
+        String title = null;
+
+        try {
+            URL url = new URL(FRED_API + "series?series_id=" + seriesId + "&api_key=" + API_KEY +
+                    "&file_type=json");
+            HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
+            InputStream inputStream = conn.getInputStream();
+
+            // Jackson JsonParser
+            JsonParser parser = new JsonFactory().createParser(inputStream);
+
+            while (!parser.isClosed()) {
+                JsonToken token = parser.nextToken();
+
+                if (token == JsonToken.FIELD_NAME && "seriess".equals(parser.getCurrentName())) {
+                    //System.out.println("Found seriess");
+
+                    assert (parser.nextToken() == JsonToken.START_ARRAY);
+
+                    while ((token = parser.nextToken()) != JsonToken.END_ARRAY) {
+                        if (token == JsonToken.FIELD_NAME && "title".equals(parser.getCurrentName())) {
+                            parser.nextToken();
+                            title = parser.getValueAsString();
+                            System.out.println("Found title: " + title);
+                            return title;
+                        }
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return title;
     }
 
     private static Map<String, Double> getData(String seriesId) {
         TreeMap<String, Double> dataMap = new TreeMap<>();
 
         // Send a request to the FRED API
-        URL url = null;
         try {
-            url = new URL("https://api.stlouisfed.org/fred/series/observations?series_id=" +
-                    seriesId + "&api_key=e12c0787f51ff6db24ac8029710fa175&file_type=json");
+            URL url = new URL(FRED_API + "series/observations?series_id=" + seriesId + "&api_key=" + API_KEY +
+                    "&file_type=json");
             HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
             InputStream inputStream = conn.getInputStream();
 
@@ -51,7 +89,7 @@ public class QueryFred {
                 }
 
                 else if (token == JsonToken.FIELD_NAME && "observations".equals(parser.getCurrentName())) {
-                    System.out.println("Found observations");
+                    //System.out.println("Found observations");
 
                     assert (parser.nextToken() == JsonToken.START_ARRAY);
 
